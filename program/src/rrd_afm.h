d06d97635018ec8f32c25dc41d6fb09838fe9e69
Tobias Oetiker <tobi@oetiker.ch>
2002-04-07 22:07:46 +0000

    added missing files from peters patch
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk@119 a5681a0c-68f1-0310-ab6d-d61299d08faa

---
+++ program/src/rrd_afm.h
@@ -0,0 +1,29 @@
+/****************************************************************************
+ * RRDtool 1.1.x  Copyright Tobias Oetiker, 1997 - 2002
+ ****************************************************************************
+ * rrd_afm.h  Parsing afm tables to find width of strings.
+ ****************************************************************************/
+
+#ifndef  RRD_AFM_H
+#define RRD_AFM_H
+
+/* measure width of a text string */
+/* fontname can be full name or postscript name */
+double afm_get_text_width ( double start, const char* font, double size,
+			    double tabwidth, const char* text);
+
+/* get postscript name from fullname or postscript name */
+const char *afm_get_font_postscript_name ( const char* font);
+
+/* cc -E -dM /dev/null */
+#ifdef __APPLE__
+/* need charset conversion from macintosh to unicode. */
+extern const unsigned char afm_mac2iso[128];
+#define afm_host2unicode(c) \
+	( (c) >= 128 ? afm_mac2iso[(c) - 128] : (c))
+#else
+/* UNSAFE macro */
+#define afm_host2unicode(a_unsigned_char) ((unsigned int)(a_unsigned_char))
+#endif
+
+#endif
