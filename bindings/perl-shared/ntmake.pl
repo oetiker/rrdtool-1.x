c2610e609c09e18a4bf435204f61e6b10d84c86b
Tobias Oetiker <tobi@oetiker.ch>
2002-05-22 05:35:09 +0000

    VC++ .NET (7.0) project files -- Eric Chamberlain <echamber@socrates.Berkeley.EDU>
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@143 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- bindings/perl-shared/ntmake.pl
+++ bindings/perl-shared/ntmake.pl
@@ -8,10 +8,12 @@ use Config;
 WriteMakefile(
     'NAME'	=> 'RRDs',
     'VERSION_FROM' => 'RRDs.pm',
-    'DEFINE'	   => "-DPERLPATCHLEVEL=$Config{PATCHLEVEL}",
+#    'DEFINE'	   => "-DPERLPATCHLEVEL=$Config{PATCHLEVEL}",
+    'DEFINE'	   => "-DPERLPATCHLEVEL=5",
+
    'INC'	=> '-I../../src/ -I../../libraries/freetype-2.0.5/include -I ../../libraries/libart_lgpl-2.3.7 -I ../../libraries/zlib-1.1.4 -I ../../libraries/libpng-1.2.0',    
 # change this path to refer to your libc.lib
-    'MYEXTLIB'  => 'c:/vc98/lib/libc.lib ../../src/release/rrd.lib ../../libraries/libart_lgpl-2.3.7/release/libart.lib ../../libraries/zlib-1.1.4/release/zlib.lib ../../libraries/libpng-1.2.0\release\png.lib ../../libraries/freetype-2.0.5/release/freetype.lib', 
+    'MYEXTLIB'  => 'e:/micros~1.net/vc7/lib/libcmt.lib ../../src/release/rrd.lib ../../libraries/libart_lgpl-2.3.7/release/libart.lib ../../libraries/zlib-1.1.4/release/zlib.lib ../../libraries/libpng-1.2.0\release\png.lib ../../libraries/freetype-2.0.5/release/freetype.lib', 
     'realclean'    => {FILES => 't/demo?.rrd t/demo?.png' },
     ($] ge '5.005') ? (
         'AUTHOR' => 'Tobias Oetiker (oetiker@ee.ethz.ch)',
