1d7625e5f00c1b65be6fa0de3a3e53912d59a075
Tobias Oetiker <tobi@oetiker.ch>
2002-07-02 10:35:23 +0000

    fixed DINF for win32
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk@154 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- program/src/rrd_nan_inf.c
+++ program/src/rrd_nan_inf.c
@@ -3,7 +3,7 @@
 #if defined(WIN32)
 
 double set_to_DNAN(void) { return (double)fmod(0.0,0.0); }
-double set_to_DINF(void) { return (double)log(0.0); }
+double set_to_DINF(void) { return (double)fabs((double)log(0.0)); }
 
 #else
 
