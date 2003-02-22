de621b04c19ef6bdc924fa061705e02c49026e60
Tobias Oetiker <tobi@oetiker.ch>
2003-02-22 21:57:03 +0000

    a patch to avoid a memory leak and a Makefile.am patch to distribute all required source files -- Peter Stamfest <peter@stamfest.at>
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@186 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/rrd_error.c
+++ src/rrd_error.c
@@ -5,6 +5,10 @@
  *****************************************************************************
  * $Id$
  * $Log$
+ * Revision 1.4  2003/02/22 21:57:03  oetiker
+ * a patch to avoid a memory leak and a Makefile.am patch to
+ * distribute all required source files -- Peter Stamfest <peter@stamfest.at>
+ *
  * Revision 1.3  2003/02/13 07:05:27  oetiker
  * Find attached the patch I promised to send to you. Please note that there
  * are three new source files (src/rrd_is_thread_safe.h, src/rrd_thread_safe.c
@@ -127,6 +131,7 @@ void
 rrd_free_context(struct rrd_context *rrd_ctx) {
     if (rrd_ctx) {
 	if (rrd_ctx->rrd_error) free(rrd_ctx->rrd_error);
+	if (rrd_ctx->lib_errstr) free(rrd_ctx->lib_errstr);
 	free(rrd_ctx);
     }
 }
