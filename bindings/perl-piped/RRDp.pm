bfe80f03f8a1bc017c02fb5eb55e42d3ee3fcd4f
Tobias Oetiker <tobi@oetiker.ch>
2005-04-04 21:07:55 +0000

    prepared for 1.2rc1 release
    git-svn-id: svn://svn.oetiker.ch/rrdtool/branches/1.2/program@348 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- bindings/perl-piped/RRDp.pm
+++ bindings/perl-piped/RRDp.pm
@@ -110,7 +110,7 @@ sub cmd (@);
 sub end ();
 sub read ();
 
-$VERSION=1.199001;
+$VERSION=1.199901;
 
 sub start ($){
   croak "rrdtool is already running"
--- bindings/perl-shared/RRDs.pm
+++ bindings/perl-shared/RRDs.pm
@@ -7,7 +7,7 @@ use vars qw(@ISA $VERSION);
 
 require DynaLoader;
 
-$VERSION=1.199001;
+$VERSION=1.199901;
 
 bootstrap RRDs $VERSION;
 
