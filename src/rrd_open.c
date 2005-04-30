fd0c1f9750eb1b8a026d7e14c673474f4d98f0c5
Tobias Oetiker <tobi@oetiker.ch>
2005-04-30 09:41:14 +0000

    even better lets have rb for CYGWIN too ... one never knows it is windows after all.
    git-svn-id: svn://svn.oetiker.ch/rrdtool/branches/1.2/program@453 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/rrd_open.c
+++ src/rrd_open.c
@@ -78,13 +78,13 @@ rrd_open(const char *file_name, FILE **in_file, rrd_t *rrd, int rdwr)
     
     rrd_init(rrd);
     if (rdwr == RRD_READONLY) {
-#if defined(WIN32) && !defined(__CYGWIN__) && !defined(__CYGWIN32__)
+#if defined(WIN32)
 	mode = "rb";
 #else
 	mode = "r";
 #endif
     } else {
-#if defined(WIN32) && !defined(__CYGWIN__) && !defined(__CYGWIN32__)
+#if defined(WIN32)
 	mode = "rb+";
 #else
 	mode = "r+";
