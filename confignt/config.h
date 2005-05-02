82a772fb68b3f528fd6175395f3d90bd70de8f40
Tobias Oetiker <tobi@oetiker.ch>
2005-05-02 20:27:13 +0000

    files that help getting that native windows port going -- Philippe.Simonet with swisscom.com
    git-svn-id: svn://svn.oetiker.ch/rrdtool/branches/1.2/program@478 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- confignt/config.h
+++ confignt/config.h
@@ -1,28 +1,54 @@
-/* config.h.nt: what configure _would_ say, if it ran on NT */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+#ifndef CONFIG_H
+#define CONFIG_H
 
-#define STDC_HEADERS 1
+#include <math.h>
+#include <float.h>
 
-/* Define if you have the strftime function.  */
+/* realloc does not support NULL as argument */
+
 #define HAVE_STRFTIME 1
-
-/* Define if you have the <math.h> header file.  */
+#define HAVE_TIME_H 1
+#define HAVE_LOCALE_H 1
+#define HAVE_TZSET 1
+#define HAVE_SETLOCALE 1
 #define HAVE_MATH_H 1
-
+#define HAVE_FLOAT_H 1
+#define HAVE_MEMMOVE 1
+#define HAVE_MALLOC_H 1
+#define HAVE_MKTIME 1
+#define HAVE_STRFTIME 1
+#define HAVE_STRING_H 1
+#define HAVE_VPRINTF 1
 #define HAVE_SYS_TYPES_H 1
 #define HAVE_SYS_STAT_H 1
 
-#define rrd_realloc(a,b) realloc((a), (b))
+//#define PACKAGE "rrdtool"
+//#define PACKAGE_NAME "rrdtool"
+//#define PACKAGE_STRING "rrdtool 1.2rc6"
+//#define PACKAGE_VERSION "1.2rc6"
+//#define VERSION "1.2rc6"
+
+#define STDC_HEADERS 1
+#define RRD_DEFAULT_FONT "arial.ttf"
 
 #define snprintf _snprintf
+#define strftime strftime_ 
 
-/* Code in rrd_graph.c:rrd_graph_init() uses the %windir% environment
- * variable to find the actual location of this relative font path to avoid
- * the recompile problem if the system directory is c:/windows vs. d:/winnt. 
- */
+#define NO_NULL_REALLOC 1
+#if NO_NULL_REALLOC
+# define rrd_realloc(a,b) ( (a) == NULL ? malloc( (b) ) : realloc( (a) , (b) ))
+#else
+# define rrd_realloc(a,b) realloc((a), (b))
+#endif      
 
-#define RRD_DEFAULT_FONT "cour.ttf"
+//#define DEBUG 1
 
+/* Vertical label angle: 90.0 (default) or 270.0 */
 #define RRDGRAPH_YLEGEND_ANGLE 90.0
 
-#define HAVE_STRING_H 1
+/* Define to 1 if you have the ANSI C header files. */
+
+
+#endif /* CONFIG_H */
 
