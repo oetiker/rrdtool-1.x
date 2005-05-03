e40679cf874d61c77027bee31621b4791dfda66d
Tobias Oetiker <tobi@oetiker.ch>
2005-05-03 20:23:05 +0000

    do not die upon wront input when in remote mode
    git-svn-id: svn://svn.oetiker.ch/rrdtool/branches/1.2/program@483 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/rrd_tool.c
+++ src/rrd_tool.c
@@ -356,7 +356,7 @@ int main(int argc, char *argv[])
 
 	    while (fgets(aLine, sizeof(aLine)-1, stdin)){
 		if ((argc = CountArgs(aLine)) == 0)  {
-		    fprintf(stderr,"ERROR: not enough arguments\n");		    
+		    fprintf(stderr,"ERROR: not enough arguments\n");
 		}
 		if ((myargv = (char **) malloc((argc+1) * 
 					       sizeof(char *))) == NULL)   {
@@ -368,8 +368,7 @@ int main(int argc, char *argv[])
 		    exit(1);
 		}
 
-		if (HandleInputLine(argc, myargv, stdout))
-		    exit(1);
+		HandleInputLine(argc, myargv, stdout);
 		free(myargv);
 
 #if HAVE_GETRUSAGE
