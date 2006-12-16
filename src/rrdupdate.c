9b2860668809f0576df7e9526660bce7cf4aaa90
Tobias Oetiker <tobi@oetiker.ch>
2006-12-16 16:01:25 +0000

    we have 2006 now
    git-svn-id: svn://svn.oetiker.ch/rrdtool/branches/1.2/program@930 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/rrdupdate.c
+++ src/rrdupdate.c
@@ -20,7 +20,7 @@ int
 main(int argc, char **argv){
         rrd_update(argc,argv);
         if (rrd_test_error()) {
-                printf("RRDtool " PACKAGE_VERSION "  Copyright by Tobi Oetiker, 1997-2005\n\n"
+                printf("RRDtool " PACKAGE_VERSION "  Copyright by Tobi Oetiker, 1997-2006\n\n"
                         "Usage: rrdupdate filename\n"
                         "\t\t\t[--template|-t ds-name:ds-name:...]\n"
                         "\t\t\ttime|N:value[:value...]\n\n"
