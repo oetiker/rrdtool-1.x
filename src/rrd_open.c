5db80415dd7ddacf46bf75d45cb4329fc0003ebc
Tobias Oetiker <tobi@oetiker.ch>
2007-09-11 06:16:52 +0000

    added msync before unmap
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@1202 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/rrd_open.c
+++ src/rrd_open.c
@@ -381,6 +381,9 @@ int rrd_close(
     int       ret;
 
 #ifdef HAVE_MMAP
+    ret = msync(rrd_file->file_start, rrd_file->file_len,MS_ASYNC);
+    if (ret != 0)
+        rrd_set_error("msync rrd_file: %s", rrd_strerror(errno));
     ret = munmap(rrd_file->file_start, rrd_file->file_len);
     if (ret != 0)
         rrd_set_error("munmap rrd_file: %s", rrd_strerror(errno));
