60b21e99828400f1f563fdd1c8fbb897fd0ca280
Tobias Oetiker <tobi@oetiker.ch>
2008-02-21 06:59:19 +0000

    added missing bits from internationalization
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@1291 a5681a0c-68f1-0310-ab6d-d61299d08faa

---
+++ src/rrd_i18n.h
@@ -0,0 +1,31 @@
+/*****************************************************************************
+ * RRDtool 1.2.19  Copyright by Takao Fujiwara, 2008
+ *****************************************************************************
+ * rrd_i18n.h   Common Header File
+ *****************************************************************************/
+#ifdef  __cplusplus
+extern "C" {
+#endif
+
+
+#ifndef _RRD_I18N_H
+#define _RRD_I18N_H
+
+#ifndef _
+/* This is for other GNU distributions with internationalized messages.
+   When compiling libc, the _ macro is predefined.  */
+#ifdef HAVE_LIBINTL_H
+#  include <libintl.h>
+#define _(String) gettext (String)
+#else
+#define _(String) (String)
+#endif
+#define N_(String) (String)
+#endif
+
+
+#endif
+
+#ifdef  __cplusplus
+}
+#endif
