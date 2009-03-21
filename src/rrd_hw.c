7d7001460ada237706351f478962c215c05918c3
Tobias Oetiker <tobi@oetiker.ch>
2009-03-21 10:44:46 +0000

    removed more rrd_flush instances
    git-svn-id: svn://svn.oetiker.ch/rrdtool/branches/1.3/program@1763 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/rrd_hw.c
+++ src/rrd_hw.c
@@ -272,7 +272,6 @@ int apply_smoother(
                 baseline[j];
         }
         /* flush cdp to disk */
-        rrd_flush(rrd_file);
         if (rrd_seek(rrd_file, sizeof(stat_head_t) +
                      rrd->stat_head->ds_cnt * sizeof(ds_def_t) +
                      rrd->stat_head->rra_cnt * sizeof(rra_def_t) +
@@ -295,7 +294,6 @@ int apply_smoother(
 
     /* endif CF_SEASONAL */
     /* flush updated values to disk */
-    rrd_flush(rrd_file);
     if (rrd_seek(rrd_file, rra_start, SEEK_SET)) {
         rrd_set_error("apply_smoother: seek to pos %d failed", rra_start);
         free(rrd_values);
@@ -310,7 +308,6 @@ int apply_smoother(
         return -1;
     }
 
-    rrd_flush(rrd_file);
     free(rrd_values);
     free(baseline);
     return 0;
