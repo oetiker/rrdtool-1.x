3ecfd0acbb318370e1fb93e7e94166df92d09c18
Tobias Oetiker <tobi@oetiker.ch>
2009-06-10 19:05:56 +0000

    add the real license ...
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@1860 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- bindings/lua/compat-5.1r5/compat-5.1.h
+++ bindings/lua/compat-5.1r5/compat-5.1.h
@@ -3,20 +3,25 @@
 ** Copyright Kepler Project 2004-2006 (http://www.keplerproject.org/compat/)
 ** $Id$
 
-Compat-5.1 is free software: it can be used for both academic and commercial
-purposes at absolutely no cost. There are no royalties or GNU-like
-"copyleft" restrictions. Compat-5.1 qualifies as Open Source software. Its
-licenses are compatible with GPL. Compat-5.1 is not in the public domain and
-the Kepler Project keep its copyright. The legal details are below.
+Copyright © 2004-2006 The Kepler Project.
   
-The spirit of the license is that you are free to use Compat-5.1 for any
-purpose at no cost without having to ask us. The only requirement is that if
-you do use Compat-5.1, then you should give us credit by including the
-appropriate copyright notice somewhere in your product or its documentation.
-  
-The Compat-5.1 library is designed and implemented by Roberto Ierusalimschy,
-Diego Nehab, André Carregal and Tomás Guisasola. The implementation is not
-derived from licensed software.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to
+deal in the Software without restriction, including without limitation the
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR  
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+IN THE SOFTWARE.   
 
 */
 
