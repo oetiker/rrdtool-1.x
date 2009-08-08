oetiker/rrdtool-1.x

commit 611c51e702c389bc7cf8b1bf36d44d11cc271a65

clearer way of advancing the flush time -- kevin
git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@1600 a5681a0c-68f1-0310-ab6d-d61299d08faa

src/rrd_daemon.c
@@ -748,8 +748,8 @@ static void *queue_thread_main (void *args __attribute__((unused))) /* {{{ */
       flush_old_values (config_write_interval);
 
       /* Determine the time of the next cache flush. */
-      while (next_flush.tv_sec <= now.tv_sec)
-        next_flush.tv_sec += config_flush_interval;
+      next_flush.tv_sec =
+        now.tv_sec + next_flush.tv_sec % config_flush_interval;
 
       /* unlock the cache while we rotate so we don't block incoming
        * updates if the fsync() blocks on disk I/O */

commit 259127fb0902ba79bcd60571d2c4300432a19f16

The current method may have caused flushes (and journal rotations) more often than necessary.  The problem was introduced in r1600. --kevin
git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@1881 a5681a0c-68f1-0310-ab6d-d61299d08faa

src/rrd_daemon.c
@@ -820,13 +820,14 @@ static void *flush_thread_main (void *args __attribute__((unused))) /* {{{ */
         || ((now.tv_sec == next_flush.tv_sec)
           && ((1000 * now.tv_usec) > next_flush.tv_nsec)))
     {
+      RRDD_LOG(LOG_DEBUG, "flushing old values");
+
+      /* Determine the time of the next cache flush. */
+      next_flush.tv_sec = now.tv_sec + config_flush_interval;
+
       /* Flush all values that haven't been written in the last
        * `config_write_interval' seconds. */
       flush_old_values (config_write_interval);
-
-      /* Determine the time of the next cache flush. */
-      next_flush.tv_sec =
-        now.tv_sec + next_flush.tv_sec % config_flush_interval;
 
       /* unlock the cache while we rotate so we don't block incoming
        * updates if the fsync() blocks on disk I/O */