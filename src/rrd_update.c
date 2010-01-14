8a4419837331e9e2d4266266c79b20b393ff117a
Tobias Oetiker <tobi@oetiker.ch>
2010-01-14 12:56:59 +0000

    re-fix #243 checking for '\0' made the whole check pointless. Now it should actually catch bad input.
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@2000 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/rrd_update.c
+++ src/rrd_update.c
@@ -1047,7 +1047,6 @@ static int update_pdp_prep(
                       || updvals[ds_idx + 1][0] > '9' )
                      && updvals[ds_idx + 1][0] != '-'
                      && updvals[ds_idx + 1][0] != 'U'
-                     && updvals[ds_idx + 1][0] == '\0'
                    ) {
                     rrd_set_error("not a simple integer: '%s'",
                                   updvals[ds_idx + 1]);
