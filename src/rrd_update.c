38b0eb0581b6ee3830b9cbe13667b6ff1cc00574
Tobias Oetiker <tobi@oetiker.ch>
2010-04-29 05:40:59 +0000

    when using CDEF datasources in rrd, the rpnp in process_pdp_st (allocated in rpn_expand) in is never freed -- Frederik Kriewitz
    git-svn-id: svn://svn.oetiker.ch/rrdtool/branches/1.4/program@2073 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/rrd_update.c
+++ src/rrd_update.c
@@ -1354,6 +1354,10 @@ static int process_pdp_st(
 
         rpnp =
             rpn_expand((rpn_cdefds_t *) &(rrd->ds_def[ds_idx].par[DS_cdef]));
+        if(rpnp == NULL) {
+          rpnstack_free(&rpnstack);
+          return -1;
+        }
         /* substitute data values for OP_VARIABLE nodes */
         for (i = 0; rpnp[i].op != OP_END; i++) {
             if (rpnp[i].op == OP_VARIABLE) {
@@ -1367,6 +1371,7 @@ static int process_pdp_st(
             rpnstack_free(&rpnstack);
             return -1;
         }
+        free(rpnp);
     }
 
     /* make pdp_prep ready for the next run */
