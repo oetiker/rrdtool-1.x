dbec288ffe14aafe81d51522a45486db0400ce45
Tobias Oetiker <tobi@oetiker.ch>
2010-06-02 20:15:20 +0000

    MAC OS X ships with binaries named 'libtool' and 'libtoolize' which are not the GNU tools. Those are prefixed with letter 'g'. --  Antoine Musso
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@2086 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-libtoolize
+
+# On MAC OS X, GNU libtoolize is named 'glibtoolize':
+if [ `(uname -s) 2>/dev/null` == 'Darwin' ]
+then
+	glibtoolize
+else
+	libtoolize
+fi
+
 autoreconf --force --install --verbose -I m4
