f0d3cccd8fa04749de5471935856760efb29b8bc
Tobias Oetiker <tobi@oetiker.ch>
2010-07-04 20:59:49 +0000

    backport FP_* fixes and revert FPCLASSIFY fix since there the FP_* values are defines.
    git-svn-id: svn://svn.oetiker.ch/rrdtool/branches/1.4/program@2101 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/rrd_config_bottom.h
+++ src/rrd_config_bottom.h
@@ -170,7 +170,7 @@ char *strchr (), *strrchr ();
 #endif
 
 /* for Solaris */
-#if (! defined(HAVE_ISINF) && defined(HAVE_FPCLASS) && defined(FP_NINF) && defined(FP_PINF)) 
+#if (! defined(HAVE_ISINF) && defined(HAVE_FPCLASS)) 
 # define HAVE_ISINF 1
 # ifdef isinf
 #  undef isinf
@@ -179,13 +179,13 @@ char *strchr (), *strrchr ();
 #endif
 
 /* solaris 10 it defines isnan such that only forte can compile it ... bad bad  */
-#if (defined(HAVE_ISNAN) && defined(isnan) && defined(HAVE_FPCLASS) && defined(FP_SNAN) && defined(FP_QNAN))
+#if (defined(HAVE_ISNAN) && defined(isnan) && defined(HAVE_FPCLASS))
 #  undef isnan
 #  define isnan(a) (fpclass(a) == FP_SNAN || fpclass(a) == FP_QNAN)
 #endif
 
 /* for OSF1 Digital Unix */
-#if (! defined(HAVE_ISINF) && defined(HAVE_FP_CLASS) && defined(HAVE_FP_CLASS_H) && defined(FP_NEG_INF) && defined( FP_POS_INF))
+#if (! defined(HAVE_ISINF) && defined(HAVE_FP_CLASS) && defined(HAVE_FP_CLASS_H))
 #  define HAVE_ISINF 1
 #  define isinf(a) (fp_class(a) == FP_NEG_INF || fp_class(a) == FP_POS_INF)
 #endif
