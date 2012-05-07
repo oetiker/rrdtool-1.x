2cd8d892e7174c730ae3cc3687a8ca67297a6c22
Tobias Oetiker <tobi@oetiker.ch>
2012-05-07 07:31:28 +0000

    fix warning: "__STRICT_ANSI__" is not defined warning patch by Gilles Esp
    git-svn-id: svn://svn.oetiker.ch/rrdtool/trunk/program@2288 a5681a0c-68f1-0310-ab6d-d61299d08faa

--- src/gettext.h
+++ src/gettext.h
@@ -170,7 +170,7 @@ npgettext_aux (const char *domain,
 #include <string.h>
 
 #define _LIBGETTEXT_HAVE_VARIABLE_SIZE_ARRAYS \
-  (((__GNUC__ >= 3 || __GNUG__ >= 2) && !__STRICT_ANSI__) \
+  (((__GNUC__ >= 3 || __GNUG__ >= 2) && !defined __STRICT_ANSI__) \
    /* || __STDC_VERSION__ >= 199901L */ )
 
 #if !_LIBGETTEXT_HAVE_VARIABLE_SIZE_ARRAYS
