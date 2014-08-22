226f0daf5afd00ab782e330295054e251059ab09
Tobias Oetiker <tobi@oetiker.ch>
2014-08-22 11:04:08 +0200

    some extra fixes

--- bindings/perl-shared/RRDs.pm
+++ bindings/perl-shared/RRDs.pm
@@ -33,6 +33,7 @@ RRDs - Access RRDtool as a shared module
   RRDs::times(start, end)
   RRDs::dump ...
   RRDs::restore ...
+  RRDs::xport ...
   RRDs::flushcached ...
 
 =head1 DESCRIPTION
@@ -129,7 +130,7 @@ integers, a pointer to an array and a pointer to a array of pointers.
     print "\n";
   }
 
-B<RRDs::xport> exposes the L<rrdxport(1)> functionality and returns data
+B<RRDs::xport> exposes the L<rrdxport|rrdxport> functionality and returns data
 with the following structure:
 
   my ($start,$end,$step,$cols,$names,$data) = RRDs::xport ...
