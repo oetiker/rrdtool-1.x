11090488bf5146feb3693ef24145d4ef5a2969c0
Tobias Oetiker <tobi@oetiker.ch>
2014-09-16 11:38:48 +0200

    the %s portion of the printf format is optional

--- src/rrd_graph.c
+++ src/rrd_graph.c
@@ -4825,7 +4825,7 @@ int bad_format_axis(char *fmt){
 }
 
 int bad_format_print(char *fmt){
-    return bad_format_check("^" SAFE_STRING FLOAT_STRING SAFE_STRING "%s" SAFE_STRING "$",fmt);
+    return bad_format_check("^" SAFE_STRING FLOAT_STRING SAFE_STRING "(?:%s)?" SAFE_STRING "$",fmt);
 }
 
 int vdef_parse(
