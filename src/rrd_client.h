20bd34df3a2ec71e91dfff2314cd457abc609f96
Sebastian Harl <sh@tokkee.org>
2016-09-24 20:01:09 +0200

    RRD client: Add an interface allowing for multiple client connections.
    Add a client object which resembles a persistent connection to a server.
    Multiple such objects may be used in parallel.
    
    The old, "simple" interface is a wrapper around the new interface now, using
    a default client connection. As before, this can only be used for one
    connection.

--- src/rrd_client.h
+++ src/rrd_client.h
@@ -42,10 +42,80 @@
 #define ENV_RRDCACHED_ADDRESS "RRDCACHED_ADDRESS"
 #define ENV_RRDCACHED_STRIPPATH "RRDCACHED_STRIPPATH"
 
-int rrdc_connect (const char *addr);
+struct rrd_client;
+typedef struct rrd_client rrd_client_t;
+
+struct rrdc_stats_s
+{
+  const char *name;
+  uint16_t type;
+#define RRDC_STATS_TYPE_GAUGE   0x0001
+#define RRDC_STATS_TYPE_COUNTER 0x0002
+  uint16_t flags;
+  union
+  {
+    uint64_t counter;
+    double   gauge;
+  } value;
+  struct rrdc_stats_s *next;
+};
+typedef struct rrdc_stats_s rrdc_stats_t;
+
+rrd_client_t *rrd_client_new(const char *addr);
+void rrd_client_destroy(rrd_client_t *client);
+
+int rrd_client_connect(rrd_client_t *client, const char *addr);
+int rrd_client_is_connected(rrd_client_t *client, const char *daemon_addr);
+int rrd_client_is_any_connected(rrd_client_t *client);
+int rrd_client_disconnect(rrd_client_t *client);
+
+int rrd_client_update(rrd_client_t *client, const char *filename, int values_num,
+    const char * const *values);
+
+rrd_info_t * rrd_client_info(rrd_client_t *client, const char *filename);
+char *rrd_client_list(rrd_client_t *client, const char *dirname);
+time_t rrd_client_last(rrd_client_t *client, const char *filename);
+time_t rrd_client_first(rrd_client_t *client, const char *filename, int rraindex);
+int rrd_client_create(rrd_client_t *client, const char *filename,
+    unsigned long pdp_step,
+    time_t last_up,
+    int no_overwrite,
+    int argc,
+    const char **argv);
+
+int rrd_client_create_r2(rrd_client_t *client, const char *filename,
+    unsigned long pdp_step,
+    time_t last_up,
+    int no_overwrite,
+    const char **sources,
+    const char *_template,
+    int argc,
+    const char **argv);
+
+int rrd_client_flush(rrd_client_t *client, const char *filename);
+int rrd_client_forget(rrd_client_t *client, const char *filename);
+int rrd_client_flush_if_daemon(rrd_client_t *client, const char *opt_daemon, const char *filename);
+int rrd_client_flushall(rrd_client_t *client);
+int rrd_client_flushall_if_daemon(rrd_client_t *client, const char *opt_daemon);
+
+int rrd_client_fetch(rrd_client_t *client, const char *filename,
+    const char *cf,
+    time_t *ret_start, time_t *ret_end,
+    unsigned long *ret_step,
+    unsigned long *ret_ds_num,
+    char ***ret_ds_names,
+    rrd_value_t **ret_data);
+
+int rrd_client_stats_get(rrd_client_t *client, rrdc_stats_t **ret_stats);
+
+/*
+ * Simple interface:
+ */
+
+int rrdc_connect(const char *addr);
 int rrdc_is_connected(const char *daemon_addr);
 int rrdc_is_any_connected(void);
-int rrdc_disconnect (void);
+int rrdc_disconnect(void);
 
 int rrdc_update (const char *filename, int values_num,
     const char * const *values);
@@ -83,22 +153,6 @@ int rrdc_fetch (const char *filename,
     unsigned long *ret_ds_num,
     char ***ret_ds_names,
     rrd_value_t **ret_data);
-
-struct rrdc_stats_s
-{
-  const char *name;
-  uint16_t type;
-#define RRDC_STATS_TYPE_GAUGE   0x0001
-#define RRDC_STATS_TYPE_COUNTER 0x0002
-  uint16_t flags;
-  union
-  {
-    uint64_t counter;
-    double   gauge;
-  } value;
-  struct rrdc_stats_s *next;
-};
-typedef struct rrdc_stats_s rrdc_stats_t;
 
 int rrdc_stats_get (rrdc_stats_t **ret_stats);
 void rrdc_stats_free (rrdc_stats_t *ret_stats);
