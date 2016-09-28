c871789f4ed0513a2f4eeb561e1657d041a9fd3e
Manoj Kumar <manoj.kumar.mbm@gmail.com>
2016-09-28 21:59:12 +0530

    Improve messages for rrdcached daemon failed due to PID Exist (#743)
    *  Improve messages for rrdcached daemon failed due to PID Exist
        New messages added in order to get clarity about the nature of problem
    
    * Review Comment incorporated regarding indentation of error messages

--- src/rrd_daemon.c
+++ src/rrd_daemon.c
@@ -524,16 +524,21 @@ static int check_pidfile(void)
   char pid_str[16];
 
   pid_fd = open_pidfile("open", O_RDWR);
-  if (pid_fd < 0)
+  if (pid_fd < 0){
+     fprintf(stderr,"FATAL: Fail to create/open PID file \n");
     return pid_fd;
+     }
 
   if (read(pid_fd, pid_str, sizeof(pid_str)) <= 0) {
+     fprintf(stderr,"FATAL: Empty PID file exist\n");
     close(pid_fd);
     return -1;
   }
 
   pid = atoi(pid_str);
   if (pid <= 0) {
+     fprintf(stderr,"FATAL: PID file is corrupted\n");
+
     close(pid_fd);
     return -1;
   }
