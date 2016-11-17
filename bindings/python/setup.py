fccd7e65a3728d9d2c513c6f7195e8d58ca2e8ad
Christian Kröger <commx@commx.ws>
2016-11-17 13:22:21 +0100

    setuptools is required to build the Python bindings

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
-try:
-    from setuptools import setup
-    from setuptools.extension import Extension
-except ImportError:
-    from distutils.core import setup, Extension
-
 import os
+
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    sys.exit('The setup requires setuptools.')
 
 TOP_SRCDIR = os.environ.get('ABS_TOP_SRCDIR', '../..')
 TOP_BUILDDIR = os.environ.get('ABS_TOP_BUILDDIR', '../..')
+
+# package version
+package_version = '0.1.10'
 
 
 def main():
@@ -21,13 +23,24 @@ def main():
 
     kwargs = dict(
         name='rrdtool',
-        version='0.1.7',
+        version=package_version,
         description='Python bindings for rrdtool',
         keywords=['rrdtool'],
         author='Christian Kroeger, Hye-Shik Chang',
         author_email='commx@commx.ws',
         license='LGPL',
         url='https://github.com/commx/python-rrdtool',
+        classifiers=['License :: OSI Approved',
+                     'Operating System :: POSIX',
+                     'Operating System :: Unix',
+                     'Operating System :: MacOS',
+                     'Programming Language :: C',
+                     'Programming Language :: Python',
+                     'Programming Language :: Python :: 2.7',
+                     'Programming Language :: Python :: 3.3',
+                     'Programming Language :: Python :: 3.4',
+                     'Programming Language :: Python :: 3.5',
+        ],
         ext_modules=[module],
         test_suite='tests'
     )
