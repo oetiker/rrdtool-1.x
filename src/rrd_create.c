oetiker/rrdtool-1.x

commit cd139a88f4994c3560b56d0a3166ebfc395e42b8

src/rrd_create.c: fix Coverity CID#32425 Insecure temporary file
        CWE-377 / https://cwe.mitre.org/data/definitions/377.html

src/rrd_create.c
@@ -1313,6 +1313,7 @@ done:
 int write_rrd(const char *outfilename, rrd_t *out) {
     int rc = -1;
     char *tmpfilename = NULL;
+    mode_t saved_umask;
 
     /* write out the new file */
 #ifdef HAVE_LIBRADOS
@@ -1337,7 +1338,10 @@ int write_rrd(const char *outfilename, rrd_t *out) {
 	strcpy(tmpfilename, outfilename);
 	strcat(tmpfilename, "XXXXXX");
 	
+	/* fix CWE-377 */
+	saved_umask = umask(S_IRUSR|S_IWUSR);
 	int tmpfd = mkstemp(tmpfilename);
+	umask(saved_umask);
 	if (tmpfd < 0) {
 	    rrd_set_error("Cannot create temporary file");
 	    goto done;

commit e1bddaf532ad29cf660318955875b1183000d850

regression caused by cd139a88f4994c3560b56d0a3166ebfc395e42b8 fix for #794

src/rrd_create.c
@@ -1344,7 +1344,7 @@ int write_rrd(const char *outfilename, rrd_t *out) {
 	strcat(tmpfilename, "XXXXXX");
 	
 	/* fix CWE-377 */
-	saved_umask = umask(S_IRUSR|S_IWUSR);
+	saved_umask = umask(S_IWGRP|S_IWOTH);
 	int tmpfd = mkstemp(tmpfilename);
 	umask(saved_umask);
 	if (tmpfd < 0) {