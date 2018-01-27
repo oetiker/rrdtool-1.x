oetiker/rrdtool-1.x

commit 8949308812a0205dd356cefbe2eb2888012ee742

Unlock the global thread lock while processing rrd
This allows other threads to run while doing I/O on rrds, improving parelellism
on multi-threaded python applications.

bindings/python/rrdtoolmodule.c
@@ -139,12 +139,16 @@ static PyObject *PyRRD_create(
 {
     PyObject *r;
     char    **argv;
-    int       argc;
+    int       argc, status;
 
     if (create_args("create", args, &argc, &argv) < 0)
         return NULL;
 
-    if (rrd_create(argc, argv) == -1) {
+    Py_BEGIN_ALLOW_THREADS
+    status = rrd_create(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (status == -1) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -168,12 +172,16 @@ static PyObject *PyRRD_update(
 {
     PyObject *r;
     char    **argv;
-    int       argc;
+    int       argc, status;
 
     if (create_args("update", args, &argc, &argv) < 0)
         return NULL;
 
-    if (rrd_update(argc, argv) == -1) {
+    Py_BEGIN_ALLOW_THREADS
+    status = rrd_update(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (status == -1) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -199,14 +207,17 @@ static PyObject *PyRRD_fetch(
     rrd_value_t *data, *datai;
     unsigned long step, ds_cnt;
     time_t    start, end;
-    int       argc;
+    int       argc, status;
     char    **argv, **ds_namv;
 
     if (create_args("fetch", args, &argc, &argv) < 0)
         return NULL;
 
-    if (rrd_fetch(argc, argv, &start, &end, &step,
-                  &ds_cnt, &ds_namv, &data) == -1) {
+    Py_BEGIN_ALLOW_THREADS
+    status = rrd_fetch(argc, argv, &start, &end, &step, &ds_cnt, &ds_namv, &data);
+    Py_END_ALLOW_THREADS
+
+    if (status == -1) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -290,14 +301,17 @@ static PyObject *PyRRD_graph(
 {
     PyObject *r;
     char    **argv, **calcpr;
-    int       argc, xsize, ysize, i;
+    int       argc, status, xsize, ysize, i;
     double    ymin, ymax;
 
     if (create_args("graph", args, &argc, &argv) < 0)
         return NULL;
 
-    if (rrd_graph(argc, argv, &calcpr, &xsize, &ysize, NULL, &ymin, &ymax) ==
-        -1) {
+    Py_BEGIN_ALLOW_THREADS
+    status = rrd_graph(argc, argv, &calcpr, &xsize, &ysize, NULL, &ymin, &ymax);
+    Py_END_ALLOW_THREADS
+
+    if (status == -1) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -344,12 +358,16 @@ static PyObject *PyRRD_tune(
 {
     PyObject *r;
     char    **argv;
-    int       argc;
+    int       argc, status;
 
     if (create_args("tune", args, &argc, &argv) < 0)
         return NULL;
 
-    if (rrd_tune(argc, argv) == -1) {
+    Py_BEGIN_ALLOW_THREADS
+    status = rrd_tune(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (status == -1) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -376,7 +394,11 @@ static PyObject *PyRRD_first(
     if (create_args("first", args, &argc, &argv) < 0)
         return NULL;
 
-    if ((ts = rrd_first(argc, argv)) == -1) {
+    Py_BEGIN_ALLOW_THREADS
+    ts = rrd_first(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (ts == -1) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -401,7 +423,11 @@ static PyObject *PyRRD_last(
     if (create_args("last", args, &argc, &argv) < 0)
         return NULL;
 
-    if ((ts = rrd_last(argc, argv)) == -1) {
+    Py_BEGIN_ALLOW_THREADS
+    ts = rrd_last(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (ts == -1) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -427,7 +453,11 @@ static PyObject *PyRRD_resize(
     if (create_args("resize", args, &argc, &argv) < 0)
         return NULL;
 
-    if ((ts = rrd_resize(argc, argv)) == -1) {
+    Py_BEGIN_ALLOW_THREADS
+    ts = rrd_resize(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (ts == -1) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -494,7 +524,11 @@ static PyObject *PyRRD_info(
     if (create_args("info", args, &argc, &argv) < 0)
         return NULL;
 
-    if ((data = rrd_info(argc, argv)) == NULL) {
+    Py_BEGIN_ALLOW_THREADS
+    data = rrd_info(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (data == NULL) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -524,7 +558,11 @@ static PyObject *PyRRD_graphv(
     if (create_args("graphv", args, &argc, &argv) < 0)
         return NULL;
 
-    if ((data = rrd_graph_v(argc, argv)) == NULL) {
+    Py_BEGIN_ALLOW_THREADS
+    data = rrd_graph_v(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (data == NULL) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -554,7 +592,11 @@ static PyObject *PyRRD_updatev(
     if (create_args("updatev", args, &argc, &argv) < 0)
         return NULL;
 
-    if ((data = rrd_update_v(argc, argv)) == NULL) {
+    Py_BEGIN_ALLOW_THREADS
+    data = rrd_update_v(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (data == NULL) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -576,13 +618,17 @@ static PyObject *PyRRD_flushcached(
     PyObject * args)
 {
     PyObject *r;
-    int       argc;
+    int       argc, status;
     char    **argv;
 
     if (create_args("flushcached", args, &argc, &argv) < 0)
         return NULL;
 
-    if (rrd_flushcached(argc, argv) != 0) {
+    Py_BEGIN_ALLOW_THREADS
+    status = rrd_flushcached(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (status != 0) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -609,7 +655,7 @@ static PyObject *PyRRD_xport(
     PyObject * args)
 {
     PyObject *r;
-    int       argc, xsize;
+    int       argc, status, xsize;
     char    **argv, **legend_v;
     time_t    start, end;
     unsigned long step, col_cnt;
@@ -618,8 +664,11 @@ static PyObject *PyRRD_xport(
     if (create_args("xport", args, &argc, &argv) < 0)
         return NULL;
 
-    if (rrd_xport(argc, argv, &xsize, &start, &end,
-                  &step, &col_cnt, &legend_v, &data) == -1) {
+    Py_BEGIN_ALLOW_THREADS
+    status = rrd_xport(argc, argv, &xsize, &start, &end, &step, &col_cnt, &legend_v, &data);
+    Py_END_ALLOW_THREADS
+
+    if (status == -1) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;
@@ -686,13 +735,17 @@ static PyObject *PyRRD_dump(
     PyObject * args)
 {
     PyObject *r;
-    int       argc;
+    int       argc, status;
     char    **argv;
 
     if (create_args("dump", args, &argc, &argv) < 0)
         return NULL;
 
-    if (rrd_dump(argc, argv) != 0) {
+    Py_BEGIN_ALLOW_THREADS
+    status = rrd_dump(argc, argv);
+    Py_END_ALLOW_THREADS
+
+    if (status != 0) {
         PyErr_SetString(ErrorObject, rrd_get_error());
         rrd_clear_error();
         r = NULL;

commit 1361a73d813920a2a6907e0eb46e7fc045187d1f

bindings: python: Fix race conditions at arguments access
rrdtool_argv and rrdtool_argc have to be allocated at each function stack in
order to have copy of variables for each running thread. Note that we call
librrd functions with GIL unlocked, so previously there was simultaneous access
to global rrdtool_argv and rrdtool_argc variables.

Fixes: 8949308812a0 ("Unlock the global thread lock while processing rrd")
Signed-off-by: Matwey V. Kornilov <matwey.kornilov@gmail.com>

bindings/python/rrdtoolmodule.c
@@ -64,9 +64,6 @@ static const char *_version = "0.1.10";
 static PyObject *rrdtool_OperationalError;
 static PyObject *rrdtool_ProgrammingError;
 
-static char **rrdtool_argv = NULL;
-static int    rrdtool_argc = 0;
-
 /**
  * PyRRD_DateTime_FromTS: convert UNIX timestamp (time_t)
  * to Python datetime object.
@@ -270,6 +267,8 @@ static char _rrdtool_create__doc__[] = "Create a new Round Robin Database.\n\n\
 static PyObject *
 _rrdtool_create(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int status;
 
@@ -307,6 +306,8 @@ static char _rrdtool_dump__doc__[] = "Dump an RRD to XML.\n\n\
 static PyObject *
 _rrdtool_dump(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int status;
 
@@ -344,6 +345,8 @@ static char _rrdtool_update__doc__[] = "Store a new set of values into\
 static PyObject *
 _rrdtool_update(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int status;
 
@@ -375,6 +378,8 @@ static char _rrdtool_updatev__doc__[] = "Store a new set of values into "\
 static PyObject *
 _rrdtool_updatev(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     rrd_info_t *data;
 
@@ -414,6 +419,8 @@ static char _rrdtool_fetch__doc__[] = "Fetch data from an RRD.\n\n\
 static PyObject *
 _rrdtool_fetch(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret, *range_tup, *dsnam_tup, *data_list, *t;
     rrd_value_t *data, *datai, dv;
     unsigned long step, ds_cnt, i, j, row;
@@ -489,6 +496,8 @@ static char _rrdtool_flushcached__doc__[] = "Flush RRD files from memory.\n\n\
 static PyObject *
 _rrdtool_flushcached(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int status;
 
@@ -581,6 +590,8 @@ static char _rrdtool_graph__doc__[] = "Create a graph based on one or more " \
 static PyObject *
 _rrdtool_graph(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int xsize, ysize, i, status;
     double ymin, ymax;
@@ -637,6 +648,8 @@ static char _rrdtool_graphv__doc__[] = "Create a graph based on one or more " \
 static PyObject *
 _rrdtool_graphv(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     rrd_info_t *data;
 
@@ -680,6 +693,8 @@ static char _rrdtool_xport__doc__[] = "Dictionary representation of data " \
 static PyObject *
 _rrdtool_xport(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int xsize, status;
     char **legend_v;
@@ -776,6 +791,8 @@ static char _rrdtool_list__doc__[] = "List RRDs in storage.\n\n" \
 static PyObject *
 _rrdtool_list(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret, *str;
     char *data, *ptr, *end;
 
@@ -836,6 +853,8 @@ static char _rrdtool_tune__doc__[] = "Modify some basic properties of a " \
 static PyObject *
 _rrdtool_tune(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int status;
 
@@ -872,6 +891,8 @@ static char _rrdtool_first__doc__[] = "Get the first UNIX timestamp of the "\
 static PyObject *
 _rrdtool_first(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int ts;
 
@@ -905,6 +926,8 @@ static char _rrdtool_last__doc__[] = "Get the UNIX timestamp of the most "\
 static PyObject *
 _rrdtool_last(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int ts;
 
@@ -940,6 +963,8 @@ static char _rrdtool_resize__doc__[] = "Modify the number of rows in a "\
 static PyObject *
 _rrdtool_resize(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     int status;
 
@@ -976,6 +1001,8 @@ static char _rrdtool_info__doc__[] = "Extract header information from an "\
 static PyObject *
 _rrdtool_info(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret;
     rrd_info_t *data;
 
@@ -1011,6 +1038,8 @@ static char _rrdtool_lastupdate__doc__[] = "Returns datetime and value stored "\
 static PyObject *
 _rrdtool_lastupdate(PyObject *Py_UNUSED(self), PyObject *args)
 {
+    char **rrdtool_argv = NULL;
+    int    rrdtool_argc = 0;
     PyObject *ret, *ds_dict, *lastupd;
     int status;
     time_t last_update;