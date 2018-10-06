0564d095146f28c65e67eae4ff0275fefabd7ce1
Alexander Zangerl <alexz@opmantek.com>
2018-10-06 12:31:08 +1000

    proposed fix for #908, no %s/%S unit autoscaling in gprint for json/xml output

--- src/rrd_xport.c
+++ src/rrd_xport.c
@@ -85,7 +85,7 @@ int rrd_xport(
     int enumds=0;
     int json=0;
     int showtime=0;
-    
+
     int opt;
     while ((opt = optparse_long(&options,longopts,NULL)) != -1){
 
@@ -850,7 +850,8 @@ static int rrd_xport_format_addprints(int flags,stringbuffer_t *buffer,image_des
   stringbuffer_t gprints={4096,0,NULL,NULL};
   char buf[256];
   char dbuf[1024];
-  char* val;
+	double magfact = -1;					/* for unit autoscaling, see %s and %S */
+  char* val, *si_symb = "";
   char* timefmt=NULL;
   if (im->xlab_user.minsec!=-1.0) { timefmt=im->xlab_user.stst; }
 
@@ -947,7 +948,27 @@ static int rrd_xport_format_addprints(int flags,stringbuffer_t *buffer,image_des
 	  ("bad format for PRINT in \"%s'", im->gdes[i].format);
 	return -1;
       } else {
-        rrd_snprintf(dbuf,sizeof(dbuf), im->gdes[i].format, printval,"");
+				 char *percent_s;
+				 /* unit autoscaling for %S and %s, logic copied from rrd_graph.c */
+				 if ((percent_s = strstr(im->gdes[i].format, "%S")) != NULL) {
+						/* Magfact is set to -1 upon function entry.  If it
+						 * is still less than 0, then we need to run auto_scale.
+						 * Otherwise, put the value into the correct units.  If
+						 * the value is 0, then do not set the symbol or magnification
+						 * so next the calculation will be performed again. */
+						if (magfact < 0.0) {
+							 auto_scale(im, &printval, &si_symb, &magfact);
+							 if (printval == 0.0)
+									magfact = -1.0;
+						} else {
+							 printval /= magfact;
+						}
+						*(++percent_s) = 's';
+				 } else if (strstr(im->gdes[i].format, "%s") != NULL) {
+						auto_scale(im, &printval, &si_symb, &magfact);
+				 }
+
+        rrd_snprintf(dbuf,sizeof(dbuf), im->gdes[i].format, printval, si_symb);
       }
       /* print */
       if (json) {
