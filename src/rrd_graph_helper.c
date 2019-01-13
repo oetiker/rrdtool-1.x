4e55ccce61cc9573b0407843ad52b79f18da7474
nirgal <nirgal@users.noreply.github.com>
2019-01-13 10:02:24 +0000

    Re-enable 0-width lines
    Some people were using these as a base for stacks.

--- src/rrd_graph_helper.c
+++ src/rrd_graph_helper.c
@@ -545,7 +545,7 @@ static graph_desc_t* newGraphDescription(image_desc_t *const im,enum gf_en gf,pa
     double linewidth = 1;
     char *t,*x;
     if ((t=getKeyValueArgument("linewidth",1,pa))&&(*t!=0)) {
-      if ((getDouble(t,&linewidth,&x))||(linewidth<=0)) {
+      if ((getDouble(t,&linewidth,&x))||(linewidth<0)) {
 	rrd_set_error("Bad line width: %s",t); return NULL;
       }
     }
