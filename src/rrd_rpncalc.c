02c2365eb2b560f66e569f4062daa64118d2bf99
Jean-Michel Vourgère <nirgal@debian.org>
2019-06-16 08:07:32 +0200

    Set fallback first_weekday to 0 (fix #1012)
    - Set first_weekday to 0 (Sunday), when HAVE__NL_TIME_WEEK_1STDAY
      is not defined
    - Fixes: https://github.com/oetiker/rrdtool-1.x/issues/1012

--- src/rrd_rpncalc.c
+++ src/rrd_rpncalc.c
@@ -564,7 +564,7 @@ static int find_first_weekday(void){
         }
         first_weekday=(week_1stday + first_weekday - 1) % 7;
 #else
-        first_weekday = 1;
+        first_weekday = 0;
 #endif
     }
     return first_weekday;
