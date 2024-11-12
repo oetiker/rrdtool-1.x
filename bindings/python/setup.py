855ea51b1c73a50a435fe681dc501ec35c0ed67f
Pierre Lebleu <pierre.lebleu@pile-engineering.com>
2024-11-12 14:57:12 +0100

    bindings: Add python3.10 support
    Signed-off-by: Pierre Lebleu <pierre.lebleu@pile-engineering.com>

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -41,6 +41,7 @@ def main():
                      'Programming Language :: Python :: 3.3',
                      'Programming Language :: Python :: 3.4',
                      'Programming Language :: Python :: 3.5',
+                     'Programming Language :: Python :: 3.10',
         ],
         ext_modules=[module],
         test_suite='tests'
